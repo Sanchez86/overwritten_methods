5756648a8f90b4d79b59d3773fc4a4d35b9c2d09
Sanchez86 <0719118@gmail.com>
2023-02-05 22:45:14 +0200

    Add check for empty array

--- array/pop.js
+++ array/pop.js
@@ -2,6 +2,10 @@ const arr = [1, 2, 3, 4, 5];
 
 const my_pop = (arr) => {
 
+    if (arr.length === 0) {
+        return undefined;
+    }
+
     let newArray = [];
 
     for (let i = 0; i < arr.length-1; i++){
